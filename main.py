fe42aa651e77e087cd2d95ee635ddbbd1e4e9d49
Showtimeeeeexitgit config --global user.email sipovichit@yandex.rugit config --global user.name Showtimeeee <sipovichit@yandex.ru>
2022-12-04 23:26:14 +0300

    block collisions, hp

--- main.py
+++ main.py
@@ -1,39 +1,54 @@
 import pygame as pg
+from random import randint
 
 pg.init()
 
 WIDTH, HEIGHT = 900, 600
 FPS = 60
 # image 32pix
-TILE = 32
+TILE = 40
 
 window = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption('TANKS')
 clock = pg.time.Clock()
+# font
+font_ui = pg.font.Font(None, 42)
 # background_image
-background_image = pg.image.load('images/pngwing.png').convert()
+background_image = pg.image.load('images/greenbg.png').convert()
 
-# x y direction offset
+# x,y direction offset
 DIRECTS = [[0, -1], [1, 0], [0, 1], [-1, 0]]
+
+
+class Interface:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    # draw health points
+    def draw(self):
+        pass
 
 
 class Tank:
     def __init__(self, color, px, py, direct, key_list):
         objects.append(self)
         self.type = 'tank'
-
         self.color = color
         self.rect = pg.Rect(px, py, TILE, TILE)
         self.direct = direct
         # speed tanks
         self.move_speed = 2
-        # helth gun
+        # health player
         self.hp = 5
         # shot delay
         self.shot_timer = 0
-        self.shotDelay = 60
+        self.shot_delay = 60
         self.bullet_speed = 5
         self.bullet_damage = 1
+
         self.key_left = key_list[0]
         self.key_right = key_list[1]
         self.key_up = key_list[2]
@@ -42,6 +57,8 @@ class Tank:
 
     # movies if button pressed
     def update(self):
+        # old pos if collicion tank
+        old_x, old_y = self.rect.topleft
         if keys[self.key_left]:
             self.rect.x -= self.move_speed
             self.direct = 3
@@ -54,15 +71,21 @@ class Tank:
         elif keys[self.key_down]:
             self.rect.y += self.move_speed
             self.direct = 2
+
+        for obj in objects:
+            if obj != self and self.rect.colliderect(obj.rect):
+                self.rect.topleft = old_x, old_y
+
         # shooting
         if keys[self.key_shot] and self.shot_timer == 0:
+            # speed bullets
             dx = DIRECTS[self.direct][0] * self.bullet_speed
             dy = DIRECTS[self.direct][1] * self.bullet_speed
             Bullet(self, self.rect.centerx, self.rect.centery, dx, dy, self.bullet_damage)
-            self.shot_timer = self.shotDelay
-
+            self.shot_timer = self.shot_delay
+        # shot delay(задержка выстрела)
         if self.shot_timer > 0:
-            self.shot_timer -= 1
+            self.shot_timer -= 0.5
 
     def draw(self):
         pg.draw.rect(window, self.color, self.rect)
@@ -89,13 +112,16 @@ class Bullet:
     def update(self):
         self.px += self.dx
         self.py += self.dy
-
+        # if pos bullet
         if self.px < 0 or self.px > WIDTH or self.py < 0 or self.py > HEIGHT:
+            # if the bullet exited the screen
             bullets.remove(self)
         else:
             for obj in objects:
+                # method checks the collision of the object with the coordinate True
                 if obj != self.parent and obj.rect.collidepoint(self.px, self.py):
                     obj.damage(self.damage)
+                    # if hits - bullets remove
                     bullets.remove(self)
                     break
 
@@ -103,11 +129,52 @@ class Bullet:
         pg.draw.circle(window, 'white', (self.px, self.py), 2)
 
 
+class Block:
+    def __init__(self, px, py, size):
+        objects.append(self)
+        self.type = 'block'
+        self.rect = pg.Rect(px, py, size, size)
+        # health block
+        self.hp = 1
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pg.draw.rect(window, 'black', self.rect)
+        pg.draw.rect(window, 'darkgrey', self.rect, 2)
+
+    def damage(self, value):
+        self.hp -= value
+        if self.hp <= value:
+            objects.remove(self)
+
+
+
 bullets = []
 # obj - color tank, coordinates, direction, buttons
 objects = []
 Tank('green', 100, 275, 0, (pg.K_a, pg.K_d, pg.K_w, pg.K_s, pg.K_SPACE))
 Tank('red', 650, 275, 0, (pg.K_LEFT, pg.K_RIGHT, pg.K_UP, pg.K_DOWN, pg.K_KP_ENTER))
+Ui = Interface()
+# block in window
+Block(100, 100, TILE)
+for b in range(60):
+    while True:
+        x = randint(0, WIDTH // TILE) * TILE
+        y = randint(1, HEIGHT // TILE) * TILE
+        rect = pg.Rect(x, y, TILE, TILE)
+        finded = False
+        for obj in objects:
+            if rect.colliderect(obj.rect):
+                finded = True
+
+        if not finded:
+            break
+    Block(x, y, TILE)
+
+
+
 
 play = True
 while play:
@@ -117,20 +184,22 @@ while play:
             play = False
     # traffic control
     keys = pg.key.get_pressed()
-
+    # update obj
     for bullet in bullets:
         bullet.update()
     for obj in objects:
         obj.update()
+    Ui.update()
 
     # window.fill('darkgrey')
     # background
     window.blit(background_image, (0, 0))
-
+    # update obj
     for bullet in bullets:
         bullet.draw()
     for obj in objects:
         obj.draw()
+    Ui.draw()
 
     pg.display.update()
     clock.tick(FPS)
